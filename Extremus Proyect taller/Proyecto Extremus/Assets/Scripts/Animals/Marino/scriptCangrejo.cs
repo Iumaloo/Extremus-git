28ebbcc7c5b0256897b0fea15099bad76c16c7a2
Andres Felipe Perez Narvaez <91978851+Thezicksss@users.noreply.github.com>
2022-04-26 14:39:47 -0500

    cambios

--- Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Animals/Marino/scriptCangrejo.cs
+++ Extremus Proyect taller/Proyecto Extremus/Assets/Scripts/Animals/Marino/scriptCangrejo.cs
@@ -16,6 +16,7 @@ public class scriptCangrejo : MonoBehaviour
     public Sprite img;
     [SerializeField]
     float tiempoDesaparicion = 10f;
+    public AnimationClip animClip;
     //waypoints
     public Transform[] waypoints;
     public int speed;
@@ -108,19 +109,25 @@ public class scriptCangrejo : MonoBehaviour
                     Invoke("AllowAction", 18f);
                     isPatrollin = true;
 
-                    //Se asigna la imagen del animal y se pone el alpha en su maximo
-                    slot.sprite = img;
-                    Color clr = slot.color;
-                    clr.a = 255f;
-                    slot.color = clr;
-                    //Funcion para limpiar el slot de la imagen despues de [tiempoDesaparicion] segudos
-                    Invoke("ClearImage", tiempoDesaparicion);
+                    //Se invoca la muestra de image despues de a duracion del clip
+                    Invoke("DisplayImage", animClip.length);
                 }
                 
             }
         }
     }
             
+    void DisplayImage()
+    {
+        //Se asigna la imagen del animal y se pone el alpha en su maximo
+        slot.sprite = img;
+        Color clr = slot.color;
+        clr.a = 255f;
+        slot.color = clr;
+        //Funcion para limpiar el slot de la imagen despues de [tiempoDesaparicion] segudos
+        Invoke("ClearImage", tiempoDesaparicion);
+    }
+
     void ClearImage()
     {
         //Quita la referencia a la imagen y pone el aplha en su minimo
